3ad1e6c9ad2e095ca1bf6394788142db45d43168
Ferdinand Lange <ferdinandlange@FerdinasLaptop2.hsd1.fl.comcast.net>
2024-07-11 15:37:55 -0400

    routing added

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,39 @@
+// import React from "react";
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home</h1>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+// import React, { useEffect } from "react";
+
+// const Home = () => {
+//   useEffect(() => {
+//     console.log("Home component rendered");
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Home</h1>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
