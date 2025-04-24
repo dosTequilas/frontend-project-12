053ce297664a2c0b52d9234ec119a526d7dae35a
Evgeniy Larin <nothinghill15@gmail.com>
2025-04-24 17:04:29 +0400

    login singlequote upd

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -10,22 +10,22 @@
 
 // export default Login;
 
-import React from "react";
-import { Formik } from "formik";
+import React from 'react';
+import { Formik } from 'formik';
 
 const Basic = () => (
   <div>
     <h1>Anywhere in your app!</h1>
     <Formik
-      initialValues={{ email: "", password: "" }}
+      initialValues={{ email: '', password: '' }}
       validate={(values) => {
         const errors = {};
         if (!values.email) {
-          errors.email = "Required";
+          errors.email = 'Required';
         } else if (
           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
         ) {
-          errors.email = "Invalid email address";
+          errors.email = 'Invalid email address';
         }
         return errors;
       }}
@@ -44,7 +44,6 @@ const Basic = () => (
         handleBlur,
         handleSubmit,
         isSubmitting,
-        /* and other goodies */
       }) => (
         <form onSubmit={handleSubmit}>
           <input
