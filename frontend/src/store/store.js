4f68635f4247373d016eb03b9120af5cc29df63e
Evgeniy Larin <nothinghill15@gmail.com>
2025-05-01 17:38:54 +0400

    chatSlice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authSlice";
-import chatReducer from "./chatSlice";
-import { channelsApi } from "./channelSlice";
-import { messagesApi } from "./messagesSlice";
-import { thunk } from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import { channelsApi } from './channelSlice';
+import { messagesApi } from './messagesSlice';
+import { thunk } from 'redux-thunk';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    chat: chatReducer,
     [messagesApi.reducerPath]: messagesApi.reducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
   },
