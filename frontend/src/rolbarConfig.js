a3e23b10b3422617bb5e88ebaebbdea205ec2eb1
Evgeniy Larin <nothinghill15@gmail.com>
2025-05-01 16:44:54 +0400

    Garbage Files Deleted

--- frontend/src/rolbarConfig.js
+++
@@ -1,8 +0,0 @@
-const rollbarConfig = {
-  accessToken: "P511aab26261d42039f5ec16675d0fd6f",
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-  environment: "testenv",
-};
-
-export default rollbarConfig;
