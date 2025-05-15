968146edb948d950a4273531820e267a5fda1944
Evgeniy Larin <nothinghill15@gmail.com>
2025-05-15 16:59:28 +0400

    eslint fixed except process

--- frontend/src/utils/init.jsx
+++ frontend/src/utils/init.jsx
@@ -1,49 +1,52 @@
-import { io } from 'socket.io-client';
-import { store } from '../store/store';
-import { messagesApi } from '../store/messagesSlice';
-import { channelsApi } from '../store/channelSlice';
-import '../i18n';
-import { I18nextProvider, initReactI18next } from 'react-i18next';
-import i18next from 'i18next';
-import { Provider } from 'react-redux';
-import { Provider as RollBarProvider } from '@rollbar/react';
+import React from 'react'
+import App from '../App'
+import { io } from 'socket.io-client'
+import { store } from '../store/store'
+import { messagesApi } from '../store/messagesSlice'
+import { channelsApi } from '../store/channelSlice'
+import '../i18n'
+import { I18nextProvider, initReactI18next } from 'react-i18next'
+import i18next from 'i18next'
+import { Provider } from 'react-redux'
+import { Provider as RollBarProvider } from '@rollbar/react'
+import resources from '../locales/ru/translation.json'
 
 const rollbarConfig = {
   accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
   environment: 'testenv',
-};
+}
 
 const init = async () => {
   const initializeApp = () => {
     // слушатели событий сервера.
-    const socket = io();
+    const socket = io()
     socket.on('newMessage', (message) => {
       store.dispatch(
         messagesApi.util.updateQueryData('getMessages', undefined, (draft) => {
-          draft.push(message);
+          draft.push(message)
         }),
-      );
-    });
+      )
+    })
 
     socket.on('newChannel', () => {
-      store.dispatch(channelsApi.util.invalidateTags(['Channels']));
-    });
+      store.dispatch(channelsApi.util.invalidateTags(['Channels']))
+    })
 
     socket.on('removeChannel', () => {
-      store.dispatch(channelsApi.util.invalidateTags(['Channels']));
-    });
+      store.dispatch(channelsApi.util.invalidateTags(['Channels']))
+    })
 
     socket.on('renameChannel', () => {
-      store.dispatch(channelsApi.util.invalidateTags(['Channels']));
-    });
-  };
-
-  const i18n = i18next.createInstance();
+      store.dispatch(channelsApi.util.invalidateTags(['Channels']))
+    })
+  }
+  initializeApp() // чтобы убрать ошибку eslint
+  const i18n = i18next.createInstance()
 
   await i18n.use(initReactI18next).init({
     resources,
     fallbackLng: 'ru',
-  });
+  })
 
   return (
     <RollBarProvider config={rollbarConfig}>
@@ -55,5 +58,7 @@ const init = async () => {
         </Provider>
       </React.StrictMode>
     </RollBarProvider>
-  );
-};
+  )
+}
+
+export default init // чтобы убрать ошибку 'init assigned but never used' в eslint
