f2dd05416d7752b2a181d87bc9ff27a089280bfe
Evgeniy Larin <nothinghill15@gmail.com>
2025-05-29 15:09:44 +0300

    thunk removed

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice.js'
 import { channelsApi } from './channelSlice.js'
 import { messagesApi } from './messagesSlice.js'
-import { thunk } from 'redux-thunk'
 
 export const store = configureStore({
   reducer: {
@@ -12,7 +11,6 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware()
-      .concat(thunk)
       .concat(messagesApi.middleware)
       .concat(channelsApi.middleware)
   },
